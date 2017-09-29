0adba7b5e8e14feef35a563284405686dcaa564b
mahmoud zougheb <mahmoudzougheb@mahmouds-MBP.attlocal.net>
2017-09-28 22:44:35 -0700

    initials

---
+++ key.js
@@ -0,0 +1,22 @@
+
+
+console.log('this is loaded');
+
+var twitterKeys = {
+  consumer_key: '<IXEMVFraMXvP9C3CS74wygRMt>',
+  consumer_secret: '<nlxYJCDkVEM3Ynd5Od32wmGIso1AVmKvvnP9uagAkS6cOa3uks>',
+  access_token_key: '<565379968-dDR21qz3bL9O68VJbOXOc2KQerSU2rWWZMvrnjkm>',
+  access_token_secret: '<K4Q1Tf9YSg0wPFCCVRqnCT3Z3sNHxwnODeri5j5QLARVa>',
+}
+
+module.exports = twitterKeys;
+
+
+
+// var spotifyKeys = {
+//   client_id: '<50a3d8454e32404eb2adf6bf7d86e1e0>',
+//   client_secret: '<b373f15b01e04dfaafcc6b2b8033f084>',
+
+// }
+
+// module.exports = spotifyKeys;
